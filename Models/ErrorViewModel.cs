1840e1a22d84f2ebc9b54e0cbbe2f606154543f1
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-07-18 10:45:01 +0300

    Created new dotnet MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace online_course_platform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
