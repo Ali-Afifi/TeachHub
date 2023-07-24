59082a4209ab7a9e3628d4c2ec500b9b1fa388e8
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-07-24 16:13:06 +0300

    Updated init.sql

--- init.sql
+++ init.sql
@@ -39,6 +39,7 @@ GO
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'1 --> male       0 --> female' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'gender'
 GO
 
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -48,7 +49,8 @@ CREATE TABLE [dbo].[Course](
 	[course_name] [varchar](200) NOT NULL,
 	[description] [varchar](max) NOT NULL,
 	[start_date] [date] NOT NULL,
-	[end_date] [date] NOT NULL
+	[end_date] [date] NOT NULL,
+	[instructor_id] [int] NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Course] ADD  CONSTRAINT [PK_Course] PRIMARY KEY CLUSTERED 
@@ -56,6 +58,12 @@ ALTER TABLE [dbo].[Course] ADD  CONSTRAINT [PK_Course] PRIMARY KEY CLUSTERED
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
+ALTER TABLE [dbo].[Course]  WITH CHECK ADD  CONSTRAINT [FK_Course_Instructor_Id] FOREIGN KEY([instructor_id])
+REFERENCES [dbo].[User] ([Id])
+GO
+ALTER TABLE [dbo].[Course] CHECK CONSTRAINT [FK_Course_Instructor_Id]
+GO
+
 
 SET ANSI_NULLS ON
 GO
