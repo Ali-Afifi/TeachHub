5b5cf8de58d934bf878d0b02d9cc7a022f2e7cc8
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-07-27 19:56:57 +0300

    Updated Controllers/StudentsController.cs * added Details() [GET] * added Enroll()  [GET] * added Enroll()  [POST]

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -8,6 +8,7 @@ using online_course_platform.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Dynamic;
+using System.Security.Cryptography.Xml;
 
 namespace online_course_platform.Controllers
 {
@@ -21,7 +22,7 @@ namespace online_course_platform.Controllers
         }
 
         // GET
-        // /Students
+        // Students
         public async Task<IActionResult> Index()
         {
 
@@ -52,5 +53,156 @@ namespace online_course_platform.Controllers
             return View(model);
 
         }
+
+
+        // GET
+        // Students/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var context = _context.Courses.Include(c => c.Instructor);
+
+
+            var courses = await context.ToListAsync();
+            var enrolls = await _context.Enrolleds.ToListAsync();
+
+
+            var alreadyEnrolled = (from enroll in enrolls
+                                   where enroll.StudentId == id
+                                   select enroll.CourseId).ToList();
+
+            var enrolleds = (from course in courses
+                            join cId in alreadyEnrolled on course.Id equals cId
+                            select new
+                            {
+                                Id = course.Id,
+                                CourseName = course.CourseName,
+                                Description = course.Description,
+                                StartDate = course.StartDate,
+                                EndDate = course.EndDate,
+                                Instructor = course.Instructor,
+                            }).ToList();
+
+
+
+            dynamic model = new ExpandoObject();
+
+            model.User = user;
+            model.Courses = enrolleds;
+            return View(model);
+        }
+
+
+        // GET
+        // Students/Enroll/5
+        public async Task<IActionResult> Enroll(int? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var context = _context.Courses.Include(c => c.Instructor);
+
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.Id == id);
+            var courses = await context.ToListAsync();
+            var enrolls = await _context.Enrolleds.ToListAsync();
+
+
+            var alreadyEnrolled = (from enroll in enrolls
+                                   where enroll.StudentId == id
+                                   select enroll.CourseId).ToList();
+
+
+            var coursesIds = (from course in courses
+                              select course.Id).ToList();
+
+
+            coursesIds = coursesIds.Except(alreadyEnrolled).ToList();
+
+            var availableCourses = (from course in courses
+                                    join cId in coursesIds on course.Id equals cId
+                                    select new
+                                    {
+                                        Id = course.Id,
+                                        CourseName = course.CourseName,
+                                        Description = course.Description,
+                                        StartDate = course.StartDate,
+                                        EndDate = course.EndDate,
+                                        Instructor = course.Instructor,
+                                    }).ToList();
+
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+
+            dynamic model = new ExpandoObject();
+
+            model.User = user;
+            model.Courses = availableCourses;
+            return View(model);
+        }
+
+        // POST
+        // Students/Enroll/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Enroll(int id, IFormCollection collection)
+        {
+            try
+            {
+
+                string[] courseInfo = collection["CourseId"].ToString().Split(" ");
+
+                var courseID = int.Parse(courseInfo[0]);
+                var instructorID = int.Parse(courseInfo[1]);
+
+
+                Enrolled enroll = new()
+                {
+                    CourseId = courseID,
+                    StudentId = id,
+                    InstructorId = instructorID,
+                };
+
+
+
+                _context.Add(enroll);
+                await _context.SaveChangesAsync();
+
+
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("------------exception-start-------");
+                System.Console.WriteLine(ex.Message);
+                System.Console.WriteLine("------------exception--end----------");
+                ViewData["Error"] = "a problem has occurred, please try again";
+
+                return View();
+            }
+        }
+
+
     }
 }
