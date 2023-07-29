8076840b526358255c5a44af288472f9cdf7c3e3
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-07-29 20:35:14 +0300

    Added Utilities/Authentication.cs

---
+++ Utilities/Authentication.cs
@@ -0,0 +1,27 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace online_course_platform.Utilities;
+
+public class Authentication
+{
+
+    public static string GenerateJwtToken(int id, string userName, string role)
+    {
+        var claims = new List<Claim> {
+            new Claim(JwtRegisteredClaimNames.Sid, id.ToString()),
+            new Claim(ClaimTypes.NameIdentifier, userName),
+            new Claim(ClaimTypes.Role, role)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MY_SECRET_KEY_123456789_ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var expires = DateTime.Now.AddMinutes(5);
+
+        var token = new JwtSecurityToken(issuer: "http://localhost", claims: claims, expires: expires, signingCredentials: credentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
