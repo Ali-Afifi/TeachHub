24d1ba181ed0e115e1cc709feca611f220f926b3
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-07-29 20:35:35 +0300

    Added Filters/SessionCheck.cs

---
+++ Filters/SessionCheck.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace online_course_platform.Filters;
+
+public class SessionCheck : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+        var httpContext = filterContext.HttpContext;
+
+        if (string.IsNullOrEmpty(httpContext.Session.GetString("_Token")))
+        {
+            filterContext.Result = new RedirectToRouteResult(
+                                            new RouteValueDictionary
+                                            {
+                                                { "Controller", "Login" },
+                                                { "Action", "" }
+                                            });
+
+        }
+    }
+
+}
