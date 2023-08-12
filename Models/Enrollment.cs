ee288a6e1a794ee8ce5cc796d357f84c201e1696
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-08-13 02:43:20 +0300

    Added Models/Enrollment.cs and Models/Teach.cs

---
+++ Models/Enrollment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace online_course_platform.Models;
+
+public partial class Enrollment
+{
+    public int UserId { get; set; }
+
+    public int CourseId { get; set; }
+
+    public int? Grade { get; set; }
+
+    public int? UpdatedBy { get; set; }
+
+    public string? IpAddressOfLastUpdate { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public string? LastUpdateOperation { get; set; }
+
+    public virtual Course User { get; set; } = null!;
+
+    public virtual User UserNavigation { get; set; } = null!;
+}
---
+++ Models/Teach.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace online_course_platform.Models;
+
+public partial class Teach
+{
+    public int CourseId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int? UpdatedBy { get; set; }
+
+    public string? IpAddressOfLastUpdate { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public string? LastUpdateOperation { get; set; }
+
+    public virtual Course Course { get; set; } = null!;
+
+    public virtual User CourseNavigation { get; set; } = null!;
+}
