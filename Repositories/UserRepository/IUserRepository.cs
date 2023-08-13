07fd343dc0f35357311a44b3ab02e2fe0965de60
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-08-13 22:18:40 +0300

    Updated Repositories/UserRepository/IUserRepository.cs

--- Repositories/UserRepository/IUserRepository.cs
+++ Repositories/UserRepository/IUserRepository.cs
@@ -4,12 +4,12 @@ namespace online_course_platform.Repositories
 {
     public interface IUserRepository
     {
-        Task<User> GetById(int id);
-        Task<User> GetByUserName(string userName);
+        Task<User?> GetById(int id);
+        Task<User?> GetByUserName(string userName);
         Task<IEnumerable<User>> GetAll();
-        void Add(User user);
-        User Update(int id, User newUser);
-        void Delete(int id);
+        Task<bool> Add(User user);
+        bool Update(User user);
+        bool Delete(User user);
 
     }
 }
