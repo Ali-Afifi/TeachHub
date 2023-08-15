8d5477843ebe2389f85163dd5ff3305a1bcc324f
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-08-15 14:24:24 +0300

    Added Services/AuthenticationService/AuthenticationService.cs

---
+++ Services/AuthenticationService/AuthenticationService.cs
@@ -0,0 +1,31 @@
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+
+namespace online_course_platform.Services;
+
+public class AuthenticationService
+{
+
+    public Password HashPassword(string password)
+    {
+        int saltLength = 16;
+        int hashLength = 32;
+        int iterationCount = 100000;
+
+        byte[] salt = RandomNumberGenerator.GetBytes(saltLength);
+
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password!,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: iterationCount,
+            numBytesRequested: hashLength));
+
+
+
+        return new Password(hashed, Convert.ToBase64String(salt));
+
+
+    }
+}
