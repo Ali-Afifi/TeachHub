89b56645edcacecc06aba0d4f1194dc99c2fca03
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-08-17 21:11:10 +0300

    Renamed file from Services/AuthenticationService/Password.cs to Services/AuthenticationService/HashedPasswordWithSalt.cs

--- Services/AuthenticationService/HashedPasswordWithSalt.cs
+++ Services/AuthenticationService/HashedPasswordWithSalt.cs
@@ -1,11 +1,11 @@
 namespace online_course_platform.Services;
 
-public record Password
+public record HashedPasswordWithSalt
 {
     public string? PasswordHash { get; init; }
     public string? Salt { get; init; }
 
-    public Password(string? passwordHash, string? salt) {
+    public HashedPasswordWithSalt(string? passwordHash, string? salt) {
         PasswordHash = passwordHash;
         Salt = salt;
     }
